d14e297c787b84a351f26082c6e61d1463092847
enriquedlc <enriquedlc2d@gmail.com>
2025-05-27 18:09:17 +0100

    :test_tube: should get the correct enchantment for fire

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,11 +1,16 @@
 import { describe, expect, it } from "vitest";
 
-import { MagicEnchantment } from "./Enchantment";
+import { MagicEnchantment } from "./MagicEnchantment";
 
 describe("should get the correct enchantment", () => {
   it("should get the correct enchantment for ice", () => {
     const enchantment = new MagicEnchantment("ice");
     expect(enchantment.prefix).toBe("Icy");
     expect(enchantment.attribute).toBe("+5 ice damage");
+  });
+  it("should get the correct enchantment for fire", () => {
+    const enchantment = new MagicEnchantment("fire");
+    expect(enchantment.prefix).toBe("Inferno");
+    expect(enchantment.attribute).toBe("+5 fire damage");
   });
 });
