58ff1ab263c53e79f9b8b0b9c45ac62c8f04a182
enriquedlc <enriquedlc2d@gmail.com>
2025-05-29 18:22:34 +0100

    :truck: rename test

--- test/MagicEnchantment.test.ts
+++ test/MagicEnchantment.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from "vitest";
 import { MagicEnchantment } from "../src/MagicEnchantment";
 
 describe("Magic Enchantment", () => {
-  it("should randomly set the enchantment", () => {
+  it("enchantment selection should be randomized", () => {
     const enchantment = new MagicEnchantment();
     expect(enchantment.prefix).toBeDefined();
     expect(enchantment.attribute).toBeDefined();
