5d5fc86a10127beb428fe1acf33be4adbb67e0b4
enriquedlc <enriquedlc2d@gmail.com>
2025-05-29 18:29:20 +0100

    :recycle: remove unnecessary test

--- test/Weapon.test.ts
+++ test/Weapon.test.ts
@@ -3,10 +3,6 @@ import { describe, expect, it } from "vitest";
 import { EnchantedWeapon } from "../src/MagicWeapon";
 
 describe("Weapon", () => {
-  it("should have an enchantment", () => {
-    const weapon = new EnchantedWeapon();
-    expect(weapon.hasEnchantment()).toBeTruthy();
-  });
   it("should have the prefix of the enchantment on them", () => {
     const weapon = new EnchantedWeapon();
     expect(weapon.stats()).toContain("Icy");
